e9d1a0694b6abcef5e0dcadef8a98ba9c7e61bb8
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2023-09-16 09:44:08 -0700

    save.

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Queue } from './data-structures/queue';
+// 1. FIFO
+// const q = new Queue(true);
+// q.push('a') // ['a']
+// q.push('b') //['a', 'b']
+
+// console.log(q.pop()) // a
+// console.log(q.pop()) // b
+
+// 2. LIFO
+// const q2 = new Queue(false)
+// q2.push('a')
+// q2.push('b')
+
+// console.log(q2.pop()) // a
+// console.log(q2.pop()) // b
+
+// 3. Priority Queue
+const q = new Queue(false)
+q.push('a', 1)
+q.push('b', 100)
+q.push('c', 10)
+q.push('d', 100)
+
+// b,d,c,a
+
+console.log(q.pop())
+console.log(q.pop())
+console.log(q.pop())
+console.log(q.pop())
+
+
